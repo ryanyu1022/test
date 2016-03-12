46c0187bc2d211e1508609015bc53db8d463a494
ryanyu <ryanyu1022@gmail.com>
2016-03-12 20:53:00 +0800

    f2 init

---
+++ test/NewViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewViewController.swift
+//  test
+//
+//  Created by ryan on 2016/3/12.
+//  Copyright © 2016年 ryan. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        print("new")
+    }
+}
